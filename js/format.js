c79cd6b3c3dfe5cfcf6f56d59f8a56b7b30fa01d
misaki80074 <misaki80074@gmail.com>
2024-08-20 16:17:03 +0800

    修改header、footer、goTop到format js檔

---
+++ js/format.js
@@ -0,0 +1,19 @@
+// header
+var menu_btn = document.getElementById("menu_btn");
+menu_btn.innerHTML = '<button class="menu">&#9776;</button>';
+
+var logo_h1 = document.getElementById("logo_h1");
+logo_h1.innerHTML = '<a href="./index.html">牌牌樂桌遊店</a>';
+
+var nav = document.getElementById("nav");
+nav.innerHTML = '<ul><li><a href="./about.html">關於我們</a></li><li><a href="./navi.html">新手指南</a></li><li><a href="./howtoplay.html">遊戲玩法</a></li><li><a href="./products.html">商品資訊</a></li><li><a href="./news.html">最新活動</a></li></ul>';
+
+
+// footer
+var footer = document.getElementById("myFooter");
+footer.innerHTML = '<p>CopyRight © 2024 牌牌樂 HAPPY CARD GAME All Rights Reserved.</p>';
+
+
+// goTop
+var goTop = document.getElementById("goTop");
+goTop.innerHTML = '<a href="#">↑<br>TOP</a>';
