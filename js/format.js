13423d250a481e6691b35d07eca99d3338d0b117
misaki80074 <misaki80074@gmail.com>
2024-08-21 16:08:44 +0800

    新增contact頁面

--- js/format.js
+++ js/format.js
@@ -6,7 +6,7 @@ var logo_h1 = document.getElementById("logo_h1");
 logo_h1.innerHTML = '<a href="./index.html">牌牌樂桌遊店</a>';
 
 var nav = document.getElementById("nav");
-nav.innerHTML = '<ul><li><a href="./about.html">關於我們</a></li><li><a href="./navi.html">新手指南</a></li><li><a href="./howtoplay.html">遊戲玩法</a></li><li><a href="./products.html">商品資訊</a></li><li><a href="./news.html">最新活動</a></li></ul>';
+nav.innerHTML = '<ul><li><a href="./about.html">關於我們</a></li><li><a href="./navi.html">新手指南</a></li><li><a href="./howtoplay.html">遊戲玩法</a></li><li><a href="./products.html">商品資訊</a></li><li><a href="./news.html">最新活動</a></li><li><a href="./contact.html">聯絡我們</a></li></ul>';
 
 
 // footer
